22af2e5d79dfe4485694ef723682fe9371f23e9c
tk0929 <bluecoffin929@gmail.com>
2021-02-02 20:24:21 +0900

    [add] #4 ログイン画面の作成

---
+++ ChatApp/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by t.koike on 2021/02/02.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextFIeld: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var dontHaveAccountButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
